7e018518b9c04962ec5365e56500d7bb3fe994a6
jimender2 <jmeredit@ashland.edu>
2018-05-14 06:23:01 -0400

    Added clear screen

--- src/game/game.java
+++ src/game/game.java
@@ -5,11 +5,12 @@ public class game {
 	public static void main(String[] args) {
 		monster monster1 = new monster(1);
 		weapon weapon1 = new weapon(1);
+		utils utils = new utils();
 		System.out.println(weapon1.getDurability());
 		System.out.println(weapon1.useWeapon());
 		
 		monster1.damage(weapon1.useWeapon());
-		
+		utils.clearScreen();
 		System.out.println("monster health " + monster1.getHealth());
 		
 		}	
--- src/game/utils.java
+++ src/game/utils.java
@@ -1,5 +1,11 @@
 package game;
 
 public class utils {
-
+	public void clearScreen(){
+		int i = 0;
+		while (i <= 15) {
+			System.out.println("");
+			i++;
+		}
+	}
 }
