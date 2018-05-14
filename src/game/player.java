72ba783d38668a504205eb778e4383509df09a90
jimender2 <jmeredit@ashland.edu>
2018-05-14 06:39:35 -0400

    Added max health

--- src/game/player.java
+++ src/game/player.java
@@ -3,18 +3,46 @@ package game;
 public class player {
 	private int level;
 	private int health;
+	private int maxHealth;
 	
 	public player() {
-		 health = 100;
+		maxHealth = 100; 
+		health = 100;
 		 level = 1;
 	 }	
 
 	public player(int lev) {
 		level = lev;
-		if (level == 1) {
-			health = 100;
-		} else if (level == 2) {
-			health = 200;
+		maxHealth = lev * 100;
+	}
+	
+	public void heal(int heal) {
+		health = health + heal;
+		if (health > maxHealth) {
+			health = maxHealth;
 		}
 	}
+	
+	public void damage(int damage) {
+		health = health - damage;
+		if (health < 0) {
+			health = 0;
+		}
+	}
+	
+	public void increaseLevel() {
+		level = level + 1;
+		maxHealth = level * 100;
+	}
+	
+	public void decreaseLevel() {
+		level = level - 1;
+		maxHealth = level * 100;
+	}
+	
+	public void setLevel(int lev) {
+		level = lev;
+		maxHealth = level * 100;
+	}
+	
 }
