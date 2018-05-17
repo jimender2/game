cb08237cc6a78b35e397a87b2b4d6212d805ff20
jimender2 <jmeredit@ashland.edu>
2018-05-16 22:37:48 -0400

    Create part of map

--- src/game/map.java
+++ src/game/map.java
@@ -4,8 +4,13 @@ import java.util.Random;
 
 public class map {
 	private int map[] = new int [10];	
+	Random random = new Random();
 	
 	public map() {
-		
+		int i = 0;
+		while (i<=9) {
+			map[i] = random.nextInt(1);
+			i++;
+		}
 	}
 }
